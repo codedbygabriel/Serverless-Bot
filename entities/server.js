44e2c2c3b25d8635f28bd34a475d17b862324ad0
Gabriel <contato.joaogabriel@proton.me>
2026-07-02 11:56:16 -0300

    feat: #loadCommands and exec commands.

--- entities/server.js
+++ entities/server.js
@@ -1,19 +1,20 @@
-import "dotenv/config";
-import {
+const {
     Client,
     GatewayIntentBits,
     Events,
     Collection,
     MessageFlags 
-} from "discord.js";
+} = require("discord.js");
+const fs = require("node:fs")
+const path = require("node:path")
+require("dotenv/config");
 
-export class ServerlessBot {
+class ServerlessBot {
     client;
-
     constructor() {
         this.#startBot();
         this.#loginBot();
-
+        this.#loadCommands();
     };
 
     #startBot() {
@@ -22,9 +23,47 @@ export class ServerlessBot {
         this.client.once(Events.ClientReady, (readyClient) => {
             console.warn(`Started as ${readyClient.user.tag}`)
         })
+
+        this.client.commands = new Collection();
     };
 
     #loginBot = () => this.client.login(process.env.DISCORD_TOKEN);
     
-    
+    async #loadCommands() {
+        const commandsDir  = path.join(__dirname, '..',  'commands');
+        const _commandsDir = fs.readdirSync(commandsDir);
+
+        for (const dir of _commandsDir) {
+            const currentDir = path.join(commandsDir, dir);
+            const filesFromDir = fs.readdirSync(currentDir).filter(f => f.endsWith('js'));
+
+            if (!(filesFromDir.length > 0)) continue;
+
+            for (const script of filesFromDir) {
+                const pathToScript = path.join(currentDir, script);
+                const _script = require(pathToScript);
+
+                this.client.commands.set(_script.data.name, _script);
+            }
+
+        }
+
+        this.client.on(Events.InteractionCreate, async (interaction) => {
+            if (!interaction.isChatInputCommand()) return;
+
+            const order = interaction.client.commands.get(interaction.commandName);
+            if (!order) return;
+            
+            try {
+                await order.execute(interaction);
+            } catch (e) {
+                if (interaction.replied || interaction.deferred)
+                    await interaction.followUp({
+                        content: "Failed to parse command.",
+                    })
+            }
+        })
+    }
 }
+
+module.exports = ServerlessBot;
