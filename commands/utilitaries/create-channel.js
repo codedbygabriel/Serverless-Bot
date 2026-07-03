c041e63208dc0275bef3ccd167f731001f612fd4
Gabriel <contato.joaogabriel@proton.me>
2026-07-03 05:44:53 -0300

    feat, refactor: moved responsabilities and added timeout to channels + better notify

--- commands/utilitaries/create-channel.js
+++ commands/utilitaries/create-channel.js
@@ -13,14 +13,22 @@ module.exports = {
                   option.setName('students')
                         .setDescription('How many students?')
                         .setRequired(true)
+              )
+              .addIntegerOption((option) => 
+                  option.setName('duration')
+                        .setDescription('Duration? Defaults to an hour.')
+                        .setRequired(false)
               ),
 
     async execute(interaction) {
         await interaction.deferReply({flags: MessageFlags.Ephemeral});
-        const channel  = interaction.options.getString('channel');
-        const students = interaction.options.getInteger('students');
-        const date     = new Date()
+
+        const channel      = interaction.options.getString('channel');
+        const students     = interaction.options.getInteger('students');
+        const duration     = interaction.options.getInteger('duration') || 1;
+        const date         = new Date()
         const channels     = interaction.guild.channels.cache;
+        const askBot       = channels.find((c) => c.name === 'ask-bot' && c.type === ChannelType.GuildText);
         const botResponses = channels.find((c) => c.name === 'bot-responses' && c.type === ChannelType.GuildText);
         const studyChannel = channels.find((c) => c.name === 'Study Channel' && c.type === ChannelType.GuildCategory);
 
@@ -29,6 +37,12 @@ module.exports = {
             return;
         }
 
+        if(interaction.channel.name !== 'ask-bot') {
+            await interaction.editReply(`${interaction.user} - Run **'${interaction.commandName}'** on ${askBot}`);
+            return;
+        }
+
+
         const varChannel = await interaction.guild.channels.create({
             name: channel,
             reason: 'No Reason Provided.',
@@ -36,6 +50,22 @@ module.exports = {
             parent: studyChannel.id,
             userLimit: students <= 99 && students > 0 ? students : 10,
         })
+
+        setTimeout(async () => {
+            try {
+                const exists = interaction.guild.channels.cache.has(varChannel.id);
+
+                if (botResponses) 
+                    await botResponses.send(`${interaction.user} - Channel ${varChannel}`)
+
+                if (exists)
+                    await varChannel.delete(`Study Session Expired`);
+
+
+            } catch (e) { 
+                console.error(e)
+            }
+        }, 3600000 * duration);
 
         await botResponses.send(`${interaction.user} - Created ${varChannel.name}!`)
         interaction.editReply(`${interaction.user} - Created ${varChannel.name} at ${date.getDay()}/${date.getMonth()} - ${date.getHours()}:${date.getMinutes()}!`);
