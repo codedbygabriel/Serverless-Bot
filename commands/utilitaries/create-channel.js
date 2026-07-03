489c32e129b298240921cd5594b3270b6bb43df1
Gabriel <contato.joaogabriel@proton.me>
2026-07-03 05:14:08 -0300

    refactor: moved channels creation to starter-kit.js

--- commands/utilitaries/create-channel.js
+++ commands/utilitaries/create-channel.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ChannelType, MessageFlags } = require("discord.js");
+const { SlashCommandBuilder, ChannelType, MessageFlags, PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,41 +17,27 @@ module.exports = {
 
     async execute(interaction) {
         await interaction.deferReply({flags: MessageFlags.Ephemeral});
-        const channel = interaction.options.getString('channel');
+        const channel  = interaction.options.getString('channel');
         const students = interaction.options.getInteger('students');
+        const date     = new Date()
+        const channels     = interaction.guild.channels.cache;
+        const botResponses = channels.find((c) => c.name === 'bot-responses' && c.type === ChannelType.GuildText);
+        const studyChannel = channels.find((c) => c.name === 'Study Channel' && c.type === ChannelType.GuildCategory);
 
-        const channels = interaction.guild.channels.cache;
+        if (!(studyChannel && botResponses)) {
+            await interaction.editReply(`${interaction.user} - Run **/starter_kit!** Your channels (Student Area, Serverless Area) are missing.`);
+            return;
+        }
 
-        let _channels = channels.find((c) => c.name === 'bot-responses' && c.type === ChannelType.GuildText);
-        let studyChannel = channels.find((c) => c.name === 'Study Channel' && c.type === ChannelType.GuildCategory);
-        let botArea = channels.find((c) => c.name === 'Serverless Area' && c.type === ChannelType.GuildCategory);
-
-        if (!_channels)
-            _channels = await interaction.guild.channels.create({
-                name: 'bot-responses',
-                type: ChannelType.GuildText,
-                parent: botArea.id,
-            })
-
-
-        if (!studyChannel)
-            studyChannel = await interaction.guild.channels.create({
-                name: 'Study Channel',
-                type: ChannelType.GuildCategory,
-            })
-        
-
-        const newVoiceChannel = interaction.guild.channels.create({
+        const varChannel = await interaction.guild.channels.create({
             name: channel,
             reason: 'No Reason Provided.',
             type: ChannelType.GuildVoice,
             parent: studyChannel.id,
             userLimit: students <= 99 && students > 0 ? students : 10,
         })
-        .then(console.log)
-        .catch(console.error)
 
-        await _channels.send(`Creating **${channel}** was a success!`)
-        interaction.editReply(`Creating **${channel}** was a success!`);
+        await botResponses.send(`${interaction.user} - Created ${varChannel.name}!`)
+        interaction.editReply(`${interaction.user} - Created ${varChannel.name} at ${date.getDay()}/${date.getMonth()} - ${date.getHours()}:${date.getMinutes()}!`);
     }
 }
